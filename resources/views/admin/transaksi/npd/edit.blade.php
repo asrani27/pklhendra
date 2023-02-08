77b45fec0f5aee6e30e8f64af27ed86b5d7e4eb9
asrani <asrani.27@gmail.com>
2023-02-08 12:20:23 +0800

    f

--- resources/views/admin/transaksi/npd/edit.blade.php
+++ resources/views/admin/transaksi/npd/edit.blade.php
@@ -69,7 +69,7 @@
                     <input type="date" class="form-control" value="{{$data->tanggal}}" name="tanggal">
                   </div>
                 </div>
-                <div class="form-group">
+                {{-- <div class="form-group">
                   <label for="inputEmail3" class="col-sm-2 control-label">Bulan</label>
                   <div class="col-sm-10">
                     <select name="bulan" class="form-control" required>
@@ -98,6 +98,12 @@
                       <option value="2024" {{$data->tahun == '2024' ? 'selected':''}}>2024</option>
                     </select>
                   </div>
+                </div> --}}
+                <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">Kode Sub Kegiatan</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" value="{{$data->kode_subkegiatan}}" name="kode_subkegiatan">
+                  </div>
                 </div>
                 <div class="form-group">
                   <label for="inputEmail3" class="col-sm-2 control-label">Sub Kegiatan</label>
@@ -105,7 +111,7 @@
                     <input type="text" class="form-control" value="{{$data->subkegiatan}}" name="subkegiatan">
                   </div>
                 </div>
-                <div class="form-group">
+                {{-- <div class="form-group">
                   <label for="inputEmail3" class="col-sm-2 control-label">NIP PPTK</label>
                   <div class="col-sm-10">
                     <input type="text" class="form-control" value="{{$data->nip_pptk}}" name="nip_pptk">
@@ -128,7 +134,7 @@
                   <div class="col-sm-10">
                     <input type="text" class="form-control" value="{{$data->pengguna}}" name="pengguna">
                   </div>
-                </div>
+                </div> --}}
                 <div class="form-group">
                   <label for="inputEmail3" class="col-sm-2 control-label"></label>
                   <div class="col-sm-10">
